6bff3579db83eab5b5e0e2076507d11499f67edb
andreysc1976 <andreysc@yandex.ru>
2022-08-07 00:50:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/a_party/mymap/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.a_party.mymap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
